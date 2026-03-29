680a8266b504fc04f4957f2436a872c2e1429414
unknown <nitric.trioxide@gmail.com>
2026-03-29 20:12:14 +0000

    Update fetch model parameters

--- Fetch_Models.bat
+++ Fetch_Models.bat
@@ -1,2 +1,2 @@
-hf download jinaai/jina-embeddings-v5-text-small-retrieval --local-dir ".\jinaai\jina-embeddings-v5-text-small" --exclude *.gguf --max-workers 12
+hf download jinaai/jina-embeddings-v5-text-small --local-dir ".\jinaai\jina-embeddings-v5-text-small" --max-workers 12
 hf download jinaai/jina-reranker-v3 --local-dir ".\jinaai\jina-reranker-v3" --max-workers 12
