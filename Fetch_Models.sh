1e8b0aae6f22e881313c4f8d6e2445274396f622
unknown <nitric.trioxide@gmail.com>
2026-05-18 19:22:35 +0000

    Add Linux-compatible Fetch_Models.sh script

---
+++ Fetch_Models.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+hf download jinaai/jina-embeddings-v5-text-small --local-dir "./jinaai/jina-embeddings-v5-text-small" --max-workers 12
+hf download jinaai/jina-reranker-v3 --local-dir "./jinaai/jina-reranker-v3" --max-workers 12
